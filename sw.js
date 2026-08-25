aff70faa3aab6846e5d1501a78b7b8e28a537376
gaigia201291-code <gaigia201291@gmail.com>
2026-08-25 09:10:48 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='tin-cafe-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json']))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{let c=x.clone();caches.open(CACHE).then(k=>k.put(e.request,c));return x}).catch(()=>caches.match('./index.html')))));
